59ffe99c385fff2be404323b2a44eaed4c04f48c
DoseiBrayin <ardilabrayan68@gmail.com>
2024-04-04 17:50:32 -0500

    (WIP) Create the Instagram conexion API to show the last eigth pictures

--- src/LandingPage/components/Instagram.jsx
+++ src/LandingPage/components/Instagram.jsx
@@ -1,23 +1,21 @@
 import React from 'react'
+import { useInstagram } from '../hooks/InstagramHook'
 
 export const Instagram = () => {
+  const data = useInstagram()
   return (
     <section className='flex flex-col items-center p-5'>
-        <h1 className='bold-heading-mobile italic'>Beach Point Moments</h1>
-        <h3 className='mb-5'>@beachpointmed</h3>
-        <div className='grid grid-cols-2 grid-rows-2 gap-4
+      <h1 className='bold-heading-mobile italic'>Beach Point Moments</h1>
+      <h3 className='mb-5'>@beachpointmed</h3>
+      <div className='grid grid-cols-2 grid-rows-2 gap-4
         md:grid-cols-4
-        
         '>
-            <img src="/LandingImages/instagram/instagram1.svg" alt="" />
-            <img src="/LandingImages/instagram/instagram1.svg" alt="" />
-            <img src="/LandingImages/instagram/instagram1.svg" alt="" />
-            <img src="/LandingImages/instagram/instagram1.svg" alt="" />
-            <img className='hidden md:block' src="/LandingImages/instagram/instagram1.svg" alt="" />
-            <img className='hidden md:block' src="/LandingImages/instagram/instagram1.svg" alt="" />
-            <img className='hidden md:block' src="/LandingImages/instagram/instagram1.svg" alt="" />
-            <img className='hidden md:block' src="/LandingImages/instagram/instagram1.svg" alt="" /> 
-        </div>
+        {
+          data && data.slice(0, 8).map((item, index) => (
+            <img key={index} className={`max-h-[298px] ${index > 3 ? 'hidden md:block' : ''}`} src={item.media_url} alt="" />
+          ))
+        }
+      </div>
     </section>
   )
 }
