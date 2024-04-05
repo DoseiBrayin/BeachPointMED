d335771576eb1b871ea8243432f6470a246a1fe6
JuanCardonaB <juanjocarbol@gmail.com>
2024-04-05 15:02:54 -0500

    (ADD FEAT): Lenguage Selector Performace

--- src/LandingPage/components/LenguageSelector.jsx
+++ src/LandingPage/components/LenguageSelector.jsx
@@ -1,44 +1,38 @@
 import React, { useState } from 'react'
 
 export const LenguageSelector = () => {
-  const [lenguageStatus, setLenguageStatus] = useState(false)
+  const sources = [
+    '/LandingImages/Countries/English.webp',
+    '/LandingImages/Countries/French.webp',
+    '/LandingImages/Countries/Portugese.webp',
+    '/LandingImages/Countries/Mandarin.webp',
+    '/LandingImages/Countries/Hindi.webp',
+    '/LandingImages/Countries/Arabic.webp',
+    '/LandingImages/Countries/Russian.webp',
+    '/LandingImages/Countries/German.webp',
+    '/LandingImages/Countries/colombia.png'
+  ]
 
-  const handleLenguage = () => {
-    setLenguageStatus(!lenguageStatus)
+  const [selectedLanguage, setSelectedLanguage] = useState({ src: sources[0], open: false })
+
+  const newListOfSources = sources.filter((source) => source !== selectedLanguage.src)
+
+  const handleLanguageSelection = (source) => {
+    setSelectedLanguage({ src: source, open: !selectedLanguage.open })
   }
+
+  const ImageStyle = 'w-[31px] h-[31px] md:w-[40px] md:h-[40px]'
+
   return (
-    <div className={`flex items-center w-[47px] md:w-[60px] flex-col ${lenguageStatus ? 'bg-[#EAEAEA] w-[47px] md:w-[60px] py-[10px] rounded-t-[5px]' : ''}`}>
-          <button onClick={() => handleLenguage()} className="w-[24px] h-[24px] md:w-[40px] md:h-[40px]">
-            <img src="public/LandingImages/Countries/colombia.png" alt="Spanish" />
-          </button>
-          {lenguageStatus && (
-            <div className="absolute items-center mt-[30px] md:mt-[40px] gap-[10px] pt-[10px] w-[47px] md:w-[60px] bg-[#EAEAEA] rounded-[5px] h-[300px] md:h-[410px] flex flex-col">
-              <button onClick={() => handleLenguage()}>
-                <img src="/LandingImages/Countries/English.webp" className="w-[24px] h-[24px] md:w-[40px] md:h-[40px] " alt="" />
-              </button>
-              <button onClick={() => handleLenguage()}>
-                <img src="/LandingImages/Countries/French.webp" className="w-[24px] h-[24px] md:w-[40px] md:h-[40px] " alt="" />
-              </button>
-              <button onClick={() => handleLenguage()}>
-                <img src="/LandingImages/Countries/Portugese.webp" className="w-[24px] h-[24px] md:w-[40px] md:h-[40px] " alt="" />
-              </button>
-              <button onClick={() => handleLenguage()}>
-                <img src="/LandingImages/Countries/Mandarin.webp" className="w-[24px] h-[24px] md:w-[40px] md:h-[40px] " alt="" />
-              </button>
-              <button onClick={() => handleLenguage()}>
-                <img src="/LandingImages/Countries/Hindi.webp" className="w-[24px] h-[24px] md:w-[40px] md:h-[40px] " alt="" />
-              </button>
-              <button onClick={() => handleLenguage()}>
-                <img src="/LandingImages/Countries/German.webp" className="w-[24px] h-[24px] md:w-[40px] md:h-[40px] " alt="" />
-              </button>
-              <button onClick={() => handleLenguage()}>
-                <img src="/LandingImages/Countries/Arabic.webp" className="w-[24px] h-[24px] md:w-[40px] md:h-[40px] " alt="" />
-              </button>
-              <button onClick={() => handleLenguage()}>
-                <img src="/LandingImages/Countries/Russian.webp" className="w-[24px] h-[24px] md:w-[40px] md:h-[40px] " alt="" />
-              </button>
-            </div>
-          )}
+    <div className={`flex items-center w-[47px] md:w-[60px] flex-col ${selectedLanguage.open ? 'bg-[#EAEAEA] w-[47px] md:w-[60px] py-[10px] rounded-t-[5px]' : ''}`}>
+      <img onClick={() => handleLanguageSelection(selectedLanguage.src)} src={selectedLanguage.src} className={ImageStyle} alt="" />
+      {selectedLanguage.open && (
+        <div className="absolute items-center mt-[35px] md:mt-[40px] gap-[10px] pt-[10px] w-[47px] md:w-[60px] bg-[#EAEAEA] rounded-[5px] h-[340px] md:h-[410px] flex flex-col">
+          {newListOfSources.map((source) => (
+            <img key={source} onClick={() => handleLanguageSelection(source)} src={source} className={ImageStyle} alt="" />
+          ))}
         </div>
+      )}
+    </div>
   )
 }
