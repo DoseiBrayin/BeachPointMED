5c925e36e04688af0a0748a647c447af209ac379
JuanCardonaB <juanjocarbol@gmail.com>
2024-04-04 19:53:46 -0500

    m

--- src/One Time Court Reservation/components/main.jsx
+++ src/One Time Court Reservation/components/main.jsx
@@ -3,29 +3,7 @@ import React from 'react'
 export const Main = () => {
   return (
     <div>
-<<<<<<< HEAD
-        <section>
-            <header>
-              <p>hola</p>
-            </header>
-            <main>
-                <h1>Select Playing Time</h1>   
-                <h1>asdasdasdsd</h1>
-                <h1>bobo</h1>
-                <h3>Date selector</h3>
-                <h1>tonto</h1>
-                <h4>Es un cambio</h4>
-                <h1>asdasdasdsd</h1>
-                <h1>asdasdasdsd</h1>
-                <h1>asdasdasdsd</h1>
-                <h1>asdasdasdsd</h1>
-                <h1>asdasdasdsd</h1>
-                <h2>bobp</h2>
-            </main>
-        </section>
-=======
       <h1>One Time Court Reservation</h1>
->>>>>>> JuanCardona
     </div>
   )
 }
