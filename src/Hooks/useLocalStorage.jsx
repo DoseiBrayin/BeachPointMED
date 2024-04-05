a95da442095447ca4005a4775b548b01cb6a1cd1
JuanCardonaB <juanjocarbol@gmail.com>
2024-04-05 15:57:44 -0500

    (ADD FEAT): Save The Country Selected In The LocalStorage

---
+++ src/Hooks/useLocalStorage.jsx
@@ -0,0 +1,23 @@
+export const useLocalStorage = ({ key }) => {
+  const setItem = (value) => {
+    try {
+      window.localStorage.setItem(key, JSON.stringify(value))
+    } catch (error) {
+      console.log(error)
+    }
+  }
+
+  const getItem = () => {
+    try {
+      const item = window.localStorage.getItem(key)
+      return item ? JSON.parse(item) : null
+    } catch (error) {
+      console.log(error)
+      return null
+    }
+  }
+  return {
+    setItem,
+    getItem
+  }
+}
--- src/LandingPage/components/LenguageSelector.jsx
+++ src/LandingPage/components/LenguageSelector.jsx
@@ -1,25 +1,40 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
+import { useLocalStorage } from '../../Hooks/useLocalStorage'
 
 export const LenguageSelector = () => {
+  // Array of images to be displayed in the selector
   const sources = [
-    '/LandingImages/Countries/English.webp',
-    '/LandingImages/Countries/French.webp',
-    '/LandingImages/Countries/Portugese.webp',
-    '/LandingImages/Countries/Mandarin.webp',
-    '/LandingImages/Countries/Hindi.webp',
-    '/LandingImages/Countries/Arabic.webp',
-    '/LandingImages/Countries/Russian.webp',
-    '/LandingImages/Countries/German.webp',
-    '/LandingImages/Countries/colombia.png'
+    'LandingImages/Countries/English.webp',
+    'LandingImages/Countries/French.webp',
+    'LandingImages/Countries/Portugese.webp',
+    'LandingImages/Countries/Mandarin.webp',
+    'LandingImages/Countries/Hindi.webp',
+    'LandingImages/Countries/Arabic.webp',
+    'LandingImages/Countries/Russian.webp',
+    'LandingImages/Countries/German.webp',
+    'LandingImages/Countries/colombia.png'
   ]
 
+  // State to manage the selected language
   const [selectedLanguage, setSelectedLanguage] = useState({ src: sources[0], open: false })
-
   const newListOfSources = sources.filter((source) => source !== selectedLanguage.src)
 
+  // Custom hook to manage the local storage of the selected language
+  const { setItem, getItem } = useLocalStorage({ key: 'language' })
+
+  // Function to handle the selection of the language and save it in the local storage
   const handleLanguageSelection = (source) => {
     setSelectedLanguage({ src: source, open: !selectedLanguage.open })
+    setItem(source)
   }
+
+  // Effect to get the selected language from the local storage
+  useEffect(() => {
+    const item = getItem()
+    if (item) {
+      setSelectedLanguage({ src: item, open: false })
+    }
+  }, [])
 
   const ImageStyle = 'w-[31px] h-[31px] md:w-[40px] md:h-[40px]'
 
