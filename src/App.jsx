9e2b356ede4d33794615ec6c47a17c7abbc31e7e
aristi1215 <juanarieda@gmail.com>
2024-04-06 20:59:55 -0500

    WIP Feat (Creacion de la vista de reservas casi terminada)

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,23 @@
 /* eslint-disable react/react-in-jsx-scope */
-import './App.css'
-import { BrowserRouter, Routes, Route } from 'react-router-dom'
-import { Page404 } from '../src/404/page/404.jsx'
-import { Landing } from './LandingPage/pages/Landing.jsx'
+import "./App.css";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { Page404 } from "../src/404/page/404.jsx";
+import { Landing } from "./LandingPage/pages/Landing.jsx";
+import { CourtReservation } from "./One Time Court Reservation/Pages/court-reservation.jsx";
+import { Navbar } from "./LandingPage/components/Navbar.jsx";
 
-function App () {
+function App() {
   return (
     <>
       <BrowserRouter>
+        <Navbar />
         <Routes>
-          <Route path="/" element={<Landing />} />
+          <Route path="/" element={<CourtReservation />} />
           <Route path="*" element={<Page404 />} />
         </Routes>
       </BrowserRouter>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
