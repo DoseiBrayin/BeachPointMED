bcb315c360a3e33e8ff993392ad42a4f4092769f
JuanCardonaB <juanjocarbol@gmail.com>
2024-04-11 15:44:45 -0500

    ADD: SelectLocation page

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,7 @@ import { Page404 } from '../src/404/page/404.jsx'
 import { Landing } from './LandingPage/pages/Landing.jsx'
 import { MyCartReservationPage } from './MyCartReservation/Pages/MyCartReservationPage.jsx'
 import { Navbar } from './components/Navbar.jsx'
+import { LocationSelection } from './LocationSelection/pages/LocationSelection.jsx'
 
 function App () {
   return (
@@ -14,6 +15,7 @@ function App () {
         <Routes>
           <Route path="/" element={<Landing />} />
           <Route path="/MyCart" element={<MyCartReservationPage />} />
+          <Route path="/LocationSelection" element={<LocationSelection />} />
           <Route path="*" element={<Page404 />} />
         </Routes>
       </BrowserRouter>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
-    "./index.html",
-    "./src/**/*.{js,ts,jsx,tsx}",
+    './index.html',
+    './src/**/*.{js,ts,jsx,tsx}'
   ],
   theme: {
     extend: {
-      colors: {
-
+      height: {
+        'screen-minus-200px': 'calc(100vh - 200px)'
       },
       fontFamily: {
-        "inter": ['Inter', 'sans-serif']
+        inter: ['Inter', 'sans-serif']
       }
-    },
+    }
   },
-  plugins: [],
+  plugins: []
 }
