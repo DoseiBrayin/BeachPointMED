2b12806b1fa9d7d8b8d5c5c814f62729b02f9edd
aristi1215 <juanarieda@gmail.com>
2024-04-11 16:40:10 -0500

    rutas actualizadas

--- src/App.jsx
+++ src/App.jsx
@@ -16,6 +16,7 @@ function App() {
           <Route path="/" element={<Landing />} />
           <Route path="/reserve" element={<Main />} />
           <Route path="/MyCart" element={<MyCartReservationPage />} />
+          <Route path="/LocationSelection" element={<LocationSelection />} />
           <Route path="*" element={<Page404 />} />
         </Routes>
       </BrowserRouter>
