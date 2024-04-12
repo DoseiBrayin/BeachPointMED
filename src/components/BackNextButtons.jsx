425181a38d04a4b54d540751ddc76fa21a61d410
JuanCardonaB <juanjocarbol@gmail.com>
2024-04-12 11:32:08 -0500

    change for letters according to client request

--- src/components/BackNextButtons.jsx
+++ src/components/BackNextButtons.jsx
@@ -9,8 +9,8 @@ export const BackNextButtons = ({ backURL, nextURL }) => {
   return (
     <div className='w-full flex justify-end max-w-[64.75rem]'>
         <div className='flex gap-3 '>
-            <Link className='border-[1px] rounded-md px-2 py-1' to={backURL}>Back</Link>
-            <Link className='border-[1px] rounded-md px-2 py-1' to={emptyLocation}>Next</Link>
+            <Link className='border-[1px] rounded-md px-2 py-1' to={backURL}>back</Link>
+            <Link className='border-[1px] rounded-md px-2 py-1' to={emptyLocation}>next</Link>
         </div>
     </div>
   )
