e974b9ed886e318da66c11b45772533cb76f57d7
Brayan Ardila <79883410+DoseiBrayin@users.noreply.github.com>
2024-04-17 08:55:53 -0500

    (WIP) Solve bug with meta env

--- src/LocationSelection/Hooks/useSelectorLocation.js
+++ src/LocationSelection/Hooks/useSelectorLocation.js
@@ -4,10 +4,8 @@ import { useBookYourCourtContext } from '../../context/BookYourCourtContext'
 
 export const useSelectorLocation = () => {
   // Define the url to make the request
-  // const token = import.meta.env.VITE_BEACHPOINT_API_URL
-  localStorage.setItem('apiUrl', import.meta.env.VITE_BEACHPOINT_API_URL)
-  const url = 'https://beachpointmed-back.onrender.com/locations/locations/'
-  const { data, loading, error } = useAxios({ url })
+  const token = import.meta.env.VITE_BEACHPOINT_API_URL
+  const { data, loading, error } = useAxios({ url: `${token}/locations/locations ` })
 
   // Get the bookCourt state and the setBookCourt function from the context
   const { bookCourt, setBookCourt } = useBookYourCourtContext()
