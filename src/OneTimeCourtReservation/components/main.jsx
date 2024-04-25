c942bdc8c22408dacb790b1a36bd70cd6e562144
Brayan Ardila <79883410+DoseiBrayin@users.noreply.github.com>
2024-04-25 15:09:31 -0500

    (FIX) data courts if not avalaible

--- src/OneTimeCourtReservation/components/main.jsx
+++ src/OneTimeCourtReservation/components/main.jsx
@@ -5,6 +5,7 @@ import { months, days } from '../Hooks/Calendar'
 import { ProgressBar } from '../../components/ProgressBar'
 import { useTimeCourts } from '../Hooks/useTimeCourts'
 import { useStartContext } from '../../context/StartCountdownContext'
+import dayjs from 'dayjs'
 
 export const Main = () => {
   const { todayState } = calendarHooks()
@@ -14,8 +15,28 @@ export const Main = () => {
     setStart(true)
   })
 
-  const { data } = useTimeCourts()
+  function formatPrice (numero) {
+    // Convertir el número a una cadena de texto y separar la parte entera de la decimal
+    const partes = numero.toString().split('.')
 
+    // Formatear la parte entera
+    const parteEntera = partes[0].replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+
+    // Si hay parte decimal
+    if (partes.length === 2) {
+      let parteDecimal = partes[1]
+
+      // Añadir ceros adicionales si la parte decimal tiene menos de tres dígitos
+      parteDecimal = parteDecimal.padEnd(3, '0')
+
+      return `${parteEntera}.${parteDecimal}`
+    } else {
+      // Si no hay parte decimal, agregar '.000'
+      return `${parteEntera}.000`
+    }
+  }
+
+  const { data } = useTimeCourts(dayjs().format('YYYY-MM-DD'))
   return (
     <section className="flex justify-center items-center">
       <div className="w-full max-w-[64.75rem] h-full px-[15px]">
@@ -44,60 +65,22 @@ export const Main = () => {
              sm:w-[100%]
              md:w-[60%]
              md:h-[28rem]"
-          >
-            <table className="w-full h-full">
-              <thead>
-                <tr>
-                  <th className="text-sm font-[600] font-inter text-center ">
-                    Time
-                  </th>
-                  <th className="text-sm font-[600] font-inter text-center lg:pl-[3rem] ">
-                    Cost
-                  </th>
-                  <th className="text-sm font-[600] font-inter text-center">
-                    Court #1
-                  </th>
-                  <th className="text-sm font-[600] font-inter text-center ">
-                    Court #2
-                  </th>
-                </tr>
-              </thead>
-              <tbody className="shadow-[-0px_0px_1px_1px_rgba(0,0,0,0.1)] divide-y-2 divide-[#EBEBEB] border border-gray-50  shadow-gray-300 rounded-2xl">
-                {
-                  data?.data.map((court) => {
-                    function formatPrice (numero) {
-                      // Convertir el número a una cadena de texto y separar la parte entera de la decimal
-                      const partes = numero.toString().split('.')
-
-                      // Formatear la parte entera
-                      const parteEntera = partes[0].replace(/\B(?=(\d{3})+(?!\d))/g, '.')
-
-                      // Si hay parte decimal
-                      if (partes.length === 2) {
-                        let parteDecimal = partes[1]
-
-                        // Añadir ceros adicionales si la parte decimal tiene menos de tres dígitos
-                        parteDecimal = parteDecimal.padEnd(3, '0')
-
-                        return `${parteEntera}.${parteDecimal}`
-                      } else {
-                        // Si no hay parte decimal, agregar '.000'
-                        return `${parteEntera}.000`
-                      }
-                    }
-
-                    return (
+          >{
+              data && Array.isArray(data.data) && data.data.length > 0
+                ? data?.data.map((court) => {
+                  return (
                     <tr key={court.id} className="rounded-tl-xl rounded-tr-xl">
                       <td className='text-sx p-2 text-center min-[425px]:text-[15px]'>
                         {court.hour}
                       </td>
                       <td className="text-xs p-2 text-center min-[425px]:text-[15px] lg:pl-[3rem]">{`${formatPrice(court.price)} COP`}</td>
                     </tr>
-                    )
-                  })
-                }
-              </tbody>
-            </table>
+                  )
+                })
+                : <tr>
+                  <td colSpan="4" className="text-center">No courts available in this date</td>
+                </tr>
+            }
           </div>
         </main>
       </div>
