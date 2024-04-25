49eddf4fa59eaf22c8e03c0953c7a11221088e9c
Brayan Ardila <79883410+DoseiBrayin@users.noreply.github.com>
2024-04-25 15:38:47 -0500

    (Fix) Fixing calendar table

--- src/OneTimeCourtReservation/components/main.jsx
+++ src/OneTimeCourtReservation/components/main.jsx
@@ -15,7 +15,7 @@ export const Main = () => {
     setStart(true)
   })
 
-  function formatPrice (numero) {
+  function formatPrice(numero) {
     // Convertir el número a una cadena de texto y separar la parte entera de la decimal
     const partes = numero.toString().split('.')
 
@@ -65,22 +65,42 @@ export const Main = () => {
              sm:w-[100%]
              md:w-[60%]
              md:h-[28rem]"
-          >{
-              data && Array.isArray(data.data) && data.data.length > 0
-                ? data?.data.map((court) => {
-                  return (
-                    <tr key={court.id} className="rounded-tl-xl rounded-tr-xl">
-                      <td className='text-sx p-2 text-center min-[425px]:text-[15px]'>
-                        {court.hour}
-                      </td>
-                      <td className="text-xs p-2 text-center min-[425px]:text-[15px] lg:pl-[3rem]">{`${formatPrice(court.price)} COP`}</td>
-                    </tr>
-                  )
-                })
-                : <tr>
-                  <td colSpan="4" className="text-center">No courts available in this date</td>
+          > <table className="w-full h-full">
+              <thead>
+                <tr>
+                  <th className="text-sm font-[600] font-inter text-center ">
+                    Time
+                  </th>
+                  <th className="text-sm font-[600] font-inter text-center lg:pl-[3rem] ">
+                    Cost
+                  </th>
+                  <th className="text-sm font-[600] font-inter text-center">
+                    Court #1
+                  </th>
+                  <th className="text-sm font-[600] font-inter text-center ">
+                    Court #2
+                  </th>
                 </tr>
-            }
+              </thead>
+              <tbody className="shadow-[-0px_0px_1px_1px_rgba(0,0,0,0.1)] divide-y-2 divide-[#EBEBEB] border border-gray-50  shadow-gray-300 rounded-2xl">
+                {
+                  data && Array.isArray(data.data) && data.data.length > 0
+                    ? data?.data.map((court) => {
+                      return (
+                        <tr key={court.id} className="rounded-tl-xl rounded-tr-xl">
+                          <td className='text-sx p-2 text-center min-[425px]:text-[15px]'>
+                            {court.hour}
+                          </td>
+                          <td className="text-xs p-2 text-center min-[425px]:text-[15px] lg:pl-[3rem]">{`${formatPrice(court.price)} COP`}</td>
+                        </tr>
+                      )
+                    })
+                    : <tr>
+                      <td colSpan="4" className="text-center">No courts available in this date</td>
+                    </tr>
+                }
+              </tbody>
+            </table>
           </div>
         </main>
       </div>
