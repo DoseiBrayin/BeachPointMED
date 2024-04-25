5b30593dffda4fc91658bbff5756d3916b95e8da
aristi1215 <juanarieda@gmail.com>
2024-04-25 15:43:29 -0500

    Add fix (Arreglos en los estilos del calendario)

--- src/OneTimeCourtReservation/components/calendar.jsx
+++ src/OneTimeCourtReservation/components/calendar.jsx
@@ -1,9 +1,6 @@
 import { generateDate, months, days } from '../Hooks/Calendar'
 import dayjs from 'dayjs'
 import { calendarHooks } from '../Hooks/calendarHooks'
-import { generateDate, months, days } from "../Hooks/Calendar";
-import dayjs from "dayjs";
-import { calendarHooks } from "../Hooks/calendarHooks";
 
 export const Calendar = () => {
   const {
@@ -16,19 +13,7 @@ export const Calendar = () => {
 
   return (
     <>
-      <ol className="w-[80%] h-[15rem] grid grid-cols-7 grid-rows-8 m-2">
-        <li className="flex col-span-full items-center justify-between w-[55%] ">
-          <img
-            className={'w-5 h-5 cursor-pointer'}
-            src="/OneTimeCourReservation/ArrowLeft.svg"
-            alt=""
-            onClick={handlePastMonth}
-          />
-          <h1 className="text-[#616161] font-[450] font-inter gap-1 text-sm w-[2rem] flex items-center justify-center text-center s">
-            {months[todayState.month()]}
-            <span> {todayState.year()}</span>
-          </h1>
-      <ol className="w-[80%] h-[15rem] grid grid-cols-7 grid-rows-8 mx-2 mt-2 justify-center items-center">
+      <ol className="w-[80%] h-[15rem] grid grid-cols-7 grid-rows-8 m-2">    
         <li className="flex col-span-full items-center justify-between w-[100%] ">
           <div className="flex justify-between w-[55%]">
             <img
@@ -67,7 +52,6 @@ export const Calendar = () => {
                 className={`
           my-[0.2rem] list-none w-[1rem] text-center h-[1rem]
           ${currentMonth ? 'text-sm cursor-pointer' : 'text-gray-400 text-xs opacity-0 cursor-context-menu'}
-                className={`my-[0.2rem] list-none w-[1rem] text-center h-[1rem]  
           ${
             currentMonth
               ? "text-sm cursor-pointer"
@@ -79,8 +63,7 @@ export const Calendar = () => {
               : ''
           }
           ${isToday ? '' : ''}
-        `}
-              >
+        `} >
                 {date.date()}
               </li>
             )
