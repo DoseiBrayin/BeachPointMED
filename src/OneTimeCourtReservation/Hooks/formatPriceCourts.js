f31d507cec896141bd3076693d8c046d196b69ab
JuanCardonaB <juanjocarbol@gmail.com>
2024-05-08 17:59:10 -0500

    Format Time Courts

---
+++ src/OneTimeCourtReservation/Hooks/formatPriceCourts.js
@@ -0,0 +1,20 @@
+export const formatPriceCourts = (price) => {
+  // Convertir el número a una cadena de texto y separar la parte entera de la decimal
+  const partes = price.toString().split('.')
+
+  // Formatear la parte entera
+  const parteEntera = partes[0].replace(/\B(?=(\d{3})+(?!\d))/g, '.')
+
+  // Si hay parte decimal
+  if (partes.length === 2) {
+    let parteDecimal = partes[1]
+
+    // Añadir ceros adicionales si la parte decimal tiene menos de tres dígitos
+    parteDecimal = parteDecimal.padEnd(3, '0')
+
+    return `${parteEntera}.${parteDecimal}`
+  } else {
+    // Si no hay parte decimal, agregar '.000'
+    return `${parteEntera}.000`
+  }
+}
---
+++ src/OneTimeCourtReservation/Hooks/formatTimeCourts.js
@@ -0,0 +1,6 @@
+export const formatTimeCourts = (time) => {
+  // Convertir el número a una cadena de texto y separar la parte entera de la decimal
+  const hora = time < 10 ? `0${time}:00` : `${time}:00`
+  const horaFinal = time >= 12 ? `${time}:00pm` : `${hora}am`
+  return horaFinal
+}
