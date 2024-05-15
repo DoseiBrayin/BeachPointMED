4f0271129c37e3571c231274c7146f11e4fec159
aristi1215 <juanarieda@gmail.com>
2024-05-15 16:32:46 -0500

    Add feat: cambios en las páginas de pago y confirmación para traer por query los datos del pago

--- src/ConfimationPage/Hooks/payment.js
+++ src/ConfimationPage/Hooks/payment.js
@@ -1,55 +1,49 @@
-import { useState } from "react";
-import { useBookYourCourtContext } from "../../context/BookYourCourtContext";
-import { set } from "react-hook-form";
+import { useBookYourCourtContext } from '../../context/BookYourCourtContext'
 
-export function Payment() {
-  const { bookCourt } = useBookYourCourtContext();
-  const [orderId, setOrderID] = useState(0);
+export function Payment () {
+  const { bookCourt } = useBookYourCourtContext()
 
   const handler = ePayco.checkout.configure({
-    key: "6e3cd97070f51ec9d8ea865d257eccc6",
-    test: true,
-  });
+    key: '6e3cd97070f51ec9d8ea865d257eccc6',
+    test: true
+  })
 
   const createOrder = () => {
-    setOrderID(orderId + 1);
     const data = {
       // Parámetros de compra (obligatorios)
-      name: "Courts: 1",
+      name: 'Courts: 1',
       description: `Courts: ${bookCourt.location.description}`,
-      invoice: `Ref_${orderId}`,
-      currency: "cop",
-      amount: "5000",
-      tax_base: "4000",
-      tax: "500",
-      tax_ico: "500",
-      country: "co",
-      lang: "en",
+      invoice: `ref-${Date.now().toString()}`,
+      currency: 'cop',
+      amount: '5000',
+      tax_base: '4000',
+      tax: '500',
+      tax_ico: '500',
+      country: 'co',
+      lang: 'es',
 
       // Onpage="false" - Standard="true"
-      //Mostrarlo como un pop up o como página entera
-      external: "false",
+      // Mostrarlo como un pop up o como página entera
+      external: 'false',
 
       // Atributos opcionales
-      extra1: "extra1",
-      extra2: "extra2",
-      extra3: "extra3",
-      //Paginas donde se recibe el estado de la compra
-      confirmation: "http://127.0.0.1:5500/src/Epayco%20integration/index.html",
-      response: "http://127.0.0.1:5500/src/Epayco%20integration/index.html",
+      extra1: 'extra1',
+      extra2: 'extra2',
+      extra3: 'extra3',
+      // Paginas donde se recibe el estado de la compra
+      confirmation: 'http://localhost:5173/LocationSelection',
+      response: 'http://localhost:5173/LocationSelection',
 
       // Atributos del cliente
-      name_billing: "Jhon Doe",
-      address_billing: "Carrera 19 numero 14 91",
-      type_doc_billing: "cc",
-      mobilephone_billing: "3050000000",
-      number_doc_billing: "100000000",
-    };
-    console.log(data.invoice)
-
-    handler.open(data);
-  };
+      name_billing: 'Jhon Doe',
+      address_billing: 'Carrera 19 numero 14 91',
+      type_doc_billing: 'cc',
+      mobilephone_billing: '3050000000',
+      number_doc_billing: '100000000'
+    }
+    handler.open(data)
+  }
   return {
-    createOrder,
-  };
+    createOrder
+  }
 }
--- src/ConfimationPage/pages/ConfirmationPage.jsx
+++ src/ConfimationPage/pages/ConfirmationPage.jsx
@@ -1,5 +1,5 @@
-import React, { useEffect } from 'react'
-import { useForm } from 'react-hook-form'
+import React, { useEffect, useState } from 'react'
+import { set, useForm } from 'react-hook-form'
 import { ErrorMessage } from '../components/ErrorMessage'
 import { Link, useNavigate } from 'react-router-dom'
 import { ProgressBar } from '../../components/ProgressBar'
