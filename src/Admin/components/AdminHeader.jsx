0f4af9d53c107dbda07d5b558b9503d950d2ff3f
JuanCardonaB <juanjocarbol@gmail.com>
2024-06-18 09:50:12 -0500

    fix Header

--- src/Admin/components/AdminHeader.jsx
+++ src/Admin/components/AdminHeader.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const AdminHeader = ({ title }) => {
   return (
-    <div className='h-[89px] px-6 pt-8'>
+    <div className='min-h-[89px] px-6 pt-8'>
         <div className='w-full flex items-center justify-between'>
             <h1 className=' font-[700] text-[40px]'>Admin Portal</h1>
             <img src="/AdminNavbar/Content.svg" alt="" />
