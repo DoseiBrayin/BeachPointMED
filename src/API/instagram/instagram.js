98ed395b21b6f026b6fcc3d48ff86881e0e3f6a9
Brayan Ardila <79883410+DoseiBrayin@users.noreply.github.com>
2024-06-19 13:00:01 -0500

    (Fix) Fixing instagram integration

--- src/API/instagram/instagram.js
+++ src/API/instagram/instagram.js
@@ -1,18 +1,16 @@
 import axios from 'axios'
 
-export async function fetchInstagram () {
+export async function fetchInstagram() {
   const token = import.meta.env.VITE_INSTAGRAM_ACCESS_TOKEN
   try {
     const response = await axios.get('https://graph.instagram.com/me/media', {
       params: {
-        fields: 'media_url',
+        fields: 'media_type,media_url',
         access_token: token
       }
     })
-    data = response.data.data.filter((item)=> item.media_url.includes('jpg'))
     return response
   } catch (error) {
     console.error(error) // Imprime el error completo
   }
 };
-
