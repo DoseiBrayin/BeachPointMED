1dc7fe9ca1027c80e5db5467b3805b39683da461
aristi1215 <juanarieda@gmail.com>
2024-06-19 14:18:50 -0500

    Add feat: Feature de whatsapp agregada, cambios en los links del footer y cambios en los links de movil

--- src/LandingPage/components/Footer.jsx
+++ src/LandingPage/components/Footer.jsx
@@ -6,6 +6,11 @@ import { useTranslation } from 'react-i18next'
 export const Footer = () => {
   const NavLinkStyles = 'text-white font-[200] text-[15px] md:text-[26px]'
   const { t } = useTranslation('global')
+
+  const handleContact = () => {
+    window.location.href = 'https://wa.me/573046678345'
+  }
+
   return (
     <div className=" h-[315px]  px-[15px] bg-[#000] flex flex-col justify-end gap-10 pb-3 md:h-[340px] md:items-center lg:px-[115px] md:gap-20">
         <div className="flex flex-col justify-between w-full md:gap-8 md:flex-row md:items-center">
@@ -15,23 +20,23 @@ export const Footer = () => {
             </div>
                 <div className="md:flex flex-col hidden">
                     <NavLink className={NavLinkStyles} to={'/'}>{t('Footer.Home')}</NavLink>
-                    <NavLink className={NavLinkStyles} to={'/'}>{t('Footer.AboutUs')}</NavLink>
-                    <NavLink className={NavLinkStyles} to={'/'}>{t('Footer.MyReservations')}</NavLink>
+                    <NavLink className={`${NavLinkStyles} hidden`} to={'/'}>{t('Footer.AboutUs')}</NavLink>
+                    <NavLink className={`${NavLinkStyles} hidden`} to={'/'}>{t('Footer.MyReservations')}</NavLink>
                 </div>
                 <div className="md:flex flex-col items-center hidden">
                     <p className="text-white font-[200] text-[15px] md:text-[26px]">{t('Footer.ContactUs')}</p>
-                    <img className="w-[39px] h-[39px] md:w-[76px] md:h-[76px]" src="/LandingImages/WhatsappLogo.webp" alt="" />
+                    <img onClick={() => handleContact()} className="cursor-pointer w-[39px] h-[39px] md:w-[76px] md:h-[76px]" src="/LandingImages/WhatsappLogo.webp" alt="" />
                 </div>
                 <button className='hidden md:flex'>
                     <img className="w-[47px] h-[71px] md:w-[78px] md:h-[117px]" src="https://pub-9ce9ae707f414f62ad3249af96d412df.r2.dev/BP-logo.webp" alt="" />
                 </button>
                 <div className='flex md:hidden justify-between px-5 mt-5'>
                     <div className="flex flex-col">
-                        <NavLink className={NavLinkStyles} to={'/'}>{t('Footer.Home')}</NavLink>
-                        <NavLink className={NavLinkStyles} to={'/'}>{t('Footer.AboutUs')}</NavLink>
-                        <NavLink className={NavLinkStyles} to={'/'}>{t('Footer.MyReservations')}</NavLink>
+                        <NavLink className={`${NavLinkStyles}`} to={'/'}>{t('Footer.Home')}</NavLink>
+                        <NavLink className={`${NavLinkStyles} hidden`} to={'/'}>{t('Footer.AboutUs')}</NavLink>
+                        <NavLink className={`${NavLinkStyles} hidden`} to={'/'}>{t('Footer.MyReservations')}</NavLink>
                     </div>
-                    <div className="flex flex-col items-center">
+                    <div className="flex flex-col items-center" onClick={() => handleContact()}>
                         <p className="text-white font-[200] text-[15px] md:text-[26px]">{t('Footer.ContactUs')}</p>
                         <img className="w-[39px] h-[39px] md:w-[76px] md:h-[76px]" src="/LandingImages/WhatsappLogo.webp" alt="" />
                     </div>
--- src/LandingPage/components/ManageReservationButton.jsx
+++ src/LandingPage/components/ManageReservationButton.jsx
@@ -3,8 +3,8 @@ import { useTranslation } from 'react-i18next'
 export const ManageReservationButton = ({ backgroundBlack = false }) => {
   const { t } = useTranslation('global')
   const style = backgroundBlack
-    ? 'border-2 border-[#fff] rounded-[25px] max-w-[279px] w-full flex justify-center h-[45px] items-center gap-2 text-white'
-    : 'border-2 border-[#000] rounded-[25px] max-w-[279px] w-full flex justify-center h-[45px] items-center gap-2 text-[#000] md:border-[#fff] md:text-white'
+    ? 'hidden border-2 border-[#fff] rounded-[25px] max-w-[279px] w-full flex justify-center h-[45px] items-center gap-2 text-white'
+    : 'hidden border-2 border-[#000] rounded-[25px] max-w-[279px] w-full flex justify-center h-[45px] items-center gap-2 text-[#000] md:border-[#fff] md:text-white'
   const image = window.innerWidth < 768 ? '/LandingImages/ManageReservationsMobile.webp' : '/LandingImages/ManageReservations.webp'
   const BackgroundImage = backgroundBlack ? '/LandingImages/ManageReservations.webp' : image
   return (
