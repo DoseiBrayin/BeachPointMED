13b8f6722167f2e282e725ec22436639df35bb36
aristi1215 <juanarieda@gmail.com>
2024-06-19 11:59:23 -0500

    Add feat: Creación de los dos charts components

--- src/Admin/Dashboard/pages/AdminDashboard.jsx
+++ src/Admin/Dashboard/pages/AdminDashboard.jsx
@@ -1,81 +1,97 @@
-import React from 'react'
 import { AdminHeader } from '../../components/AdminHeader'
+import { OverviewComponent } from '../components/OverviewComponent'
+import { CourtRevenue } from '../components/CourtRevenue'
+import { BarChart, Bar, Rectangle, XAxis, YAxis, Tooltip, Legend, ResponsiveContainer } from 'recharts'
+import MockReservations from '../../mocks/reserveData.json'
+import GaugeChart from '../components/gaugeChart'
+
+const data = [
+  {
+    month: 'january',
+    sells: 64000
+  },
+  {
+    month: 'febrero',
+    sells: 6000
+  },
+  {
+    month: 'marzo',
+    sells: 7000
+  },
+  {
+    month: 'abril',
+    sells: 10000
+  },
+  {
+    month: 'mayo',
+    sells: 43000
+  },
+  {
+    month: 'junio',
+    sells: 8000
+  }
+]
 
 export const AdminDashboard = () => {
+  const availableCourts = 7 // Ejemplo de canchas disponibles
+  const totalCourts = 10
   return (
     <section className='h-[100vh] flex flex-col'>
       <header>
       <AdminHeader title='Dashboard' />
       </header>
-      <main className='w-full h-fit flex-grow flex flex-col bg-gray-200 gap-4'>
+      <main className='w-full h-fit flex-grow flex flex-col bg-[#eef0f2] gap-4'>
           <div className="h-[17%] p-3 flex flex-col bg-white rounded-lg mx-3 mt-3 ">
-            <h1>Overview</h1>
+            <h1 className='font-inter text-[#383E49] font-[500] text-xl'>Overview</h1>
             <div className='flex justify-between items-center flex-grow'>
-              <div className='w-[23%] flex place-items-end gap-3'>
-                <div>
-                <h4>Today&apos;s</h4>
-                <h3>Courts reserved</h3>
-                </div>
-                <h2>23</h2>
-              </div>
-              <div className='w-[23%] flex place-items-end gap-3'>
-                <div>
-                <h4>Today&apos;s</h4>
-                <h3>Courts reserved</h3>
-                </div>
-                <h2>23</h2>
-              </div>
-              <div className='w-[23%] flex place-items-end gap-3'>
-                <div>
-                <h4>Today&apos;s</h4>
-                <h3>Courts reserved</h3>
-                </div>
-                <h2>23</h2>
-              </div>
-              <div className='w-[23%] flex place-items-end gap-3'>
-                <div>
-                <h4>Today&apos;s</h4>
-                <h3>Courts reserved</h3>
-                </div>
-                <h2>23</h2>
-              </div>
+             <OverviewComponent date={"Today's"} text={'Courts reserved'} />
+             <OverviewComponent date={"Today's"} text={'Courts available'} />
+             <OverviewComponent date={'Made today'} text={'One-time reservations'} />
+             <OverviewComponent date={'Made today'} text={'Recurring Reservations'} />
             </div>
 
           </div>
 
-          <div className="h-[27%] bg-white rounded-lg mx-3 flex flex-col p-3">
-            <h1>Court revenue</h1>
+          <div className="h-[30%] bg-white rounded-lg mx-3 flex flex-col p-3">
+            <h1 className='font-inter text-[#383E49] font-[500] text-xl'>Court Revenue</h1>
             <div className='flex flex-grow justify-between items-center gap-4'>
-
-            <div className='w-[23%] border border-black rounded-lg'>
-              <h3>Titulo h3</h3>
-              <h2>Titulo h2</h2>
-              <strong>Texto strong</strong>
-            </div>
-
-            <div className='w-[23%] border border-black rounded-lg'>
-              <h3>Titulo h3</h3>
-              <h2>Titulo h2</h2>
-              <strong>Texto strong</strong>
-            </div>
-
-            <div className='w-[23%] border border-black rounded-lg'>
-              <h3>Titulo h3</h3>
-              <h2>Titulo h2</h2>
-              <strong>Texto strong</strong>
-            </div>
-
-            <div className='w-[23%] border border-black rounded-lg'>
-              <h3>Titulo h3</h3>
-              <h2>Titulo h2</h2>
-              <strong>Texto strong</strong>
-            </div>
-
+              <CourtRevenue location={'Ciudad del río'} courtNumber={'Court #1'} price={'2.042.059 '} />
+              <CourtRevenue location={'Belen'} courtNumber={'Court #2'} price={'22.042.059 '} />
+              <CourtRevenue location={'Bogota'} courtNumber={'Court #1'} price={'2.042.059 '} />
+              <CourtRevenue location={'Llerales'} courtNumber={'Court #2'} price={'2.042.059 '} />
             </div>
           </div>
 
-          <div className="h-[47%] flex bg-white rounded-lg mx-3">
-          </div>
+          <div className="h-[45%] flex ">
+            <div className='h-full w-full bg-white rounded-lg mx-3'>
+                <div>
+                <h1>booking Status</h1>
+                </div>
+                <ResponsiveContainer width="100%" height="100%" aspect={5} >
+                  <BarChart
+                    width={500}
+                    height={300}
+                    data={data}
+                    margin={{
+                      top: 5,
+                      right: 30,
+                      left: 20,
+                      bottom: 5
+                    }}
+                  >
+                    <XAxis dataKey="month" />
+                    <YAxis />
+                    <Tooltip />
+                    <Legend />
+                    <Bar dataKey="sells" fill="#8884d8" activeBar={<Rectangle fill="pink" stroke="blue" />} />
+                  </BarChart>
+              </ResponsiveContainer>
+              </div>
+              <div className='h-full w-1/2 flex flex-col justify-around items-center  bg-center bg-white rounded-lg mx-3'>
+                <h1>booking Status</h1>
+                  <GaugeChart value={availableCourts} maxValue={totalCourts} />
+              </div>
+            </div>
       </main>
     </section>
   )
